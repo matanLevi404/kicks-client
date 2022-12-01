c6dfb92aad94632b3f89166bb40485698328fd79
matanLevi404 <matan.levi.404@gmail.com>
2022-12-01 16:11:06 +0200

    pord port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
   production: true,
-  apiURL: 'http://localhost:1001',
+  apiURL: 'https://kicks-server.onrender.com',
   ANGULAR_APP_PUBLISHABLE_KEY:
     'pk_test_51M8K6zDb07Qcgu34NfxVkZRs9MxQhZtKacgkGsDTzveSA8Y3DVPlw3RTq9jInGRIEEOwih6GzmPohdDrFqdhRm6j00O6igSdSY',
 };
